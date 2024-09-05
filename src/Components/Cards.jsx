63d3b81e32c513aba108f2f7b9b0217ad4412357
kunalPrajapati <130208908+Kunal-prajapati-1@users.noreply.github.com>
2024-09-05 10:51:32 +0530

    Update Cards.jsx

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import Card from './Card'
-
+import logo001 from './logos/logo001.svg';
+import logo002 from './logos/logo002.svg';
+import logo003 from './logos/logo003.png';
 const Cards = () => {
     const data =[
-        {bg:'#004D43',width:"50%", src:"src\\Components\\logos\\logo001.svg",button:'©2019-2022'},
-        {bg:'#212121',width:"25%", src:'src\\Components\\logos\\logo002.svg',button:'rating 5.0 on clutch'},
-        {bg:'#212121',width:"25%", src:'src\\Components\\logos\\logo003.png',button:'business bootcamp alumni'},
+        {bg:'#004D43',width:"50%", src: logo001,button:'©2019-2022'},
+        {bg:'#212121',width:"25%", src: logo002,button:'rating 5.0 on clutch'},
+        {bg:'#212121',width:"25%", src: logo003,button:'business bootcamp alumni'},
     ]
   return (
     <div className='max-h-screen w-full flex sm:flex-col sm:items-center base:flex-row gap-[1vw] bg-[#F1F1F1] px-[4vw] py-[8vw]'>
